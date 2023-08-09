eedac8b09890f2cf3282cac4cc02e8944c5406ce
lucio <creppenderman@gmail.com>
2023-08-09 02:46:56 -0300

    adapted keys.js for gamepad controller

--- crackthecoin/static/js/keys.js
+++ crackthecoin/static/js/keys.js
@@ -3,51 +3,104 @@ const elements = document.querySelectorAll(".capture");
 
 // Set the initial focus index to the first element
 let currentFocusIndex = 0;
-let cooldown = false;
-const cooldownTime = 200; // Set the cooldown time in milliseconds
 let current_element = elements[currentFocusIndex];
 current_element.classList.add("focused");
 
-document.addEventListener("keydown", (event) => {
-  const key = event.key;
-
-  if (cooldown) {
-    return; // If cooldown is active, don't do anything
-  }
-
-
-  // Update the currentFocusIndex based on the arrow key pressed
-  if ((key === "ArrowDown") && currentFocusIndex < elements.length - 1) {
-    // Remove the 'focused' class from the currently focused element
-    current_element.classList.remove("focused");
-    currentFocusIndex++;
-    current_element = elements[currentFocusIndex];
-  } else if ((key === "ArrowUp") && currentFocusIndex > 0) {
-    // Remove the 'focused' class from the currently focused element
-    current_element.classList.remove("focused");
-    currentFocusIndex--;
-    current_element = elements[currentFocusIndex];
-  } else if (key === "Enter") {
-    // If the user presses Enter, click the currently focused element
-    current_element.click();
-  }else {
-    return;
-  }
-
-  // Add the 'focused' class to the new focused element
-  current_element.classList.add("focused");
-  setTimeout(() => {current_element.scrollIntoView({ behavior: "smooth" });}, 200);
+// gamepad variables
+let gamepad;
+let controllerIndex = 0;
+let releasedButtons = {
+  "A": true,
+  "B": true,
+  "Up": true,
+  "Down": true,
+  "Left": true,
+  "Right": true,
+};
 
 
-  current_element.focus();
-
-
-  cooldown = true;
-  setTimeout(() => {
-    cooldown = false;
-  }, cooldownTime);
-
-  console.log(currentFocusIndex);
+window.addEventListener("gamepadconnected", (event) => {
+  console.log("A gamepad connected:");
+  console.log(event.gamepad);
+  const gamepad = event.gamepad;
+  controllerIndex = gamepad.index;
 });
 
-// ------------------------------
+window.addEventListener("gamepaddisconnected", (event) => {
+  console.log("A gamepad disconnected:");
+  console.log(event.gamepad);
+});
+
+function backspace() {
+  console.log("Backspace pressed");
+  //check if is an input text and if it has some text
+  if (current_element.tagName == "INPUT" && current_element.type == "text" && current_element.value.length > 0) {
+    current_element.value = current_element.value.slice(0, -1);
+  }
+}
+
+function update() {
+  gamepad = navigator.getGamepads()[controllerIndex];
+
+  if (gamepad) {
+    if (gamepad.buttons[0].pressed && releasedButtons["A"]) {
+      console.log("A pressed");
+      current_element.click();
+      releasedButtons["A"] = false;
+    } else if (!gamepad.buttons[0].pressed) {
+      releasedButtons["A"] = true;
+    }
+
+    if (gamepad.buttons[1].pressed && releasedButtons["B"]) {
+      console.log("B pressed");
+      backspace();
+      releasedButtons["B"] = false;
+    } else if (!gamepad.buttons[1].pressed) {
+      releasedButtons["B"] = true;
+    }
+
+    if (gamepad.axes[0] > 0.5 && releasedButtons["Up"]) {
+      console.log("Up pressed");
+      currentFocusIndex--;
+      releasedButtons["Up"] = false;
+    } else if (gamepad.axes[0] < 0.5) {
+      releasedButtons["Up"] = true;
+    }
+
+    if (gamepad.axes[0] < -0.5 && releasedButtons["Down"]) {
+      console.log("Down pressed");
+      currentFocusIndex++;
+      releasedButtons["Down"] = false;
+    } else if (gamepad.axes[0] > -0.5) {
+      releasedButtons["Down"] = true;
+    }
+
+    if (gamepad.axes[1] > 0.5 && releasedButtons["Right"]) {
+      console.log("Right pressed");
+      releasedButtons["Right"] = false;
+    } else if (gamepad.axes[1] <= 0.5) {
+      releasedButtons["Right"] = true;
+    }
+
+    if (gamepad.axes[1] < -0.5 && releasedButtons["Left"]) {
+      console.log("Left pressed");
+      releasedButtons["Left"] = false;
+    } else if (gamepad.axes[1] >= -0.5) {
+      releasedButtons["Left"] = true;
+    }
+
+    // Add the 'focused' class to the new focused element
+    if (currentFocusIndex < 0) {
+      currentFocusIndex = elements.length - 1;
+    } else if (currentFocusIndex >= elements.length) {
+      currentFocusIndex = 0;
+    }
+    current_element.classList.remove("focused");
+    current_element = elements[currentFocusIndex];
+    current_element.classList.add("focused");
+    current_element.focus();
+  }
+  requestAnimationFrame(update);
+}
+
+requestAnimationFrame(update);
