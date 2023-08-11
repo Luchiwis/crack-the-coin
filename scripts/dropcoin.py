08ffe296bbe4dae9cd7f9a466bcad58027f16f61
lucio <creppenderman@gmail.com>
2023-08-11 06:37:55 -0300

    fixed dropcoin

--- scripts/dropcoin.py
+++ scripts/dropcoin.py
@@ -5,20 +5,18 @@ import time
 
 
 def dropcoin():
-    arduino_port = list(serial.tools.list_ports.comports())[0]
-    if arduino_port.device != 'COM1':
-        print('Arduino is connected')
-    else:
-        print('ERROR: Arduino is NOT connected')
-        return False
-    
-    ser = serial.Serial(arduino_port.device, 9600)
-    time.sleep(2)
+    for arduino_port in list(serial.tools.list_ports.comports()):
+        if arduino_port.device != 'COM1':
+            print('Arduino is connected')
+            ser = serial.Serial(arduino_port.device, 9600)
+            time.sleep(2)
 
-    ser.write("1".encode())
-    print("Coin dropped")
+            ser.write("1".encode())
+            print("Coin dropped")
+            
+            ser.close()
     
-    ser.close()
+    
 
 if __name__ == "__main__":
     dropcoin()
